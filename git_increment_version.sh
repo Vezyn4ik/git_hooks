d70e2c58cf0afa5e505ede46da0ba66cc72cb87f
Vladyslav Batrachenko <vladyslav_batrachenko@epam.com>
2023-05-05 21:30:03 +0300

    feat(Main-Task-Module-4): Build tools - update increment_version v1

--- git_increment_version.sh
+++ git_increment_version.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# Increment the minor version
-old_version=$(grep -oP '(?<=version ).*' build.gradle)
-minor_version=$(echo $old_version | grep -oP '(?<=\.).*')
-new_minor_version=$(expr $minor_version + 1)
-new_version=$(echo $old_version | sed "s/\.[0-9]*$/.${new_minor_version}/")
+# get the minor version
+minorVersion=$(cat gradle.properties | grep minorVersion | cut -d= -f2)
 
+minorVersion=$(( $minorVersion+1))
+sed -i "s/^minorVersion=.*/minorVersion=$minorVersion/" gradle.properties
+echo "Minor version: $minorVersion"
 # Commit the version change and create a Git tag
-git add build.gradle version.properties
-#git commit -m "Bump version to ${new_version}"
-git tag -a ${new_version} -m "Version ${new_version}"
+git add gradle.properties
+git commit -m "Bump version to ${minorVersion}"
+git tag -a "${minorVersion}" -m "Version ${minorVersion}"
+
+read -p "Press enter"
